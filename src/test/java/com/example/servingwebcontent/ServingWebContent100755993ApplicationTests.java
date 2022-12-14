7c7a7ec6827a0d767ef12d76b5b7b976256abefe
MJ Galbraith <murray.galbraith@dcmail.ca>
2022-12-14 14:22:26 -0500

    Initial commit

---
+++ src/test/java/com/example/servingwebcontent/ServingWebContent100755993ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContent100755993ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
